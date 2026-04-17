a106fc7a7fc6a4a289820a1b9078a6ae8348bab8
MartinRifo <martinrifo65@gmail.com>
2026-04-17 18:11:41 -0400

    ejercicios for

---
+++ for.py
@@ -0,0 +1,77 @@
+#explicacion y uso de for:
+
+# for i in range/¡(6)
+# print ("hola")
+
+# num=int(input("ingrese un numero: "))
+
+# for i in range(num):
+#    print(i+1,"repeticion")
+
+
+
+
+
+   #pedir un numero al usuario y mostrar su tabla de multiplicar
+
+# num=int(input("ingrese un numero"))
+
+# for i in range(12):
+#     print(num, "x" , i+1, "=", num*(i+1))
+    
+
+# titulo= "clima de hoy"
+# diadelmes=17
+# mes=4
+# tem=25.7
+# llueve=True
+
+#     print(titulo, "temperatura", tem,diadelmes,"7",mes)
+
+# if llueve==True
+
+# else ("no sacar paraguas")
+
+# preguntar cuantas notas son y sacar el promedio
+# num=int(input("ingrese un numero: "))  
+# suma=0
+# for i in range (num):
+#     nota=float(input("ingrese la nota: "))
+#     suma=suma+nota
+#     prom= suma/num
+#     print("el promedio es: ", prom)
+
+#     if prom>=4:
+#         print("aprobado")
+#     else:
+#         print("reprobado")
+
+#sumatoria
+# num=int(input("ingrese un numero")
+#         total=0
+#         for i in range(num):
+#             total=total*i+1
+#             print("la sumatoria es: ",total)
+# contador de vocales y consonantes
+# vocales=0
+# conso=o
+# print("ingrese su nombre: ")
+# nombre=input()
+# if i== "a" or i== "e" or i== "i" or i== "o" or i== "u":
+#         print("es una vocal")
+
+# for i in  (nombre):
+#     print(i)
+#     vocales=vocales+1
+# elif i == "":
+# else:
+#     conso=conso+1
+    
+
+# print("la cantidad de vocales es: ",vocales)
+# print("la cantidad de consonantes es: ",conso)
+
+
+
+    
+
